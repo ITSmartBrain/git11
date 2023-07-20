5ac22065ce1ab34bf3bec612a66911e2d769fc66
al.tukanov@gmail.com <al.tukanov@gmail.com>
2023-07-20 21:10:19 +0400

    добавлено деление

--- src/p1/Main.java
+++ src/p1/Main.java
@@ -9,6 +9,7 @@ public class Main {
                 1. Сложить
                 2. Вычесть
                 3. Умножить
+                4. Делить
                 """);
         int choice = scn.nextInt();
         if(choice == 1) {
@@ -26,6 +27,12 @@ public class Main {
             int b = scn.nextInt();
             System.out.println(a*b);
         }
+        else if(choice == 4){
+            int a = scn.nextInt();
+            int b = scn.nextInt();
+            System.out.println(a/b);
+        }
+
         else{
             System.out.println("Неверный выбор");
         }
