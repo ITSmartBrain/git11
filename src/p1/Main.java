fcace0ddee46089ffeba140d02e2e545c492818f
al.tukanov@gmail.com <al.tukanov@gmail.com>
2023-07-20 21:14:58 +0400

    clear

--- src/p1/Main.java
+++ src/p1/Main.java
@@ -6,36 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("""
-                1. Сложить
-                2. Вычесть
-                3. Умножить
-                4. Делить
+                
                 """);
         int choice = scn.nextInt();
-        if(choice == 1) {
-            int a = scn.nextInt();
-            int b = scn.nextInt();
-            System.out.println(a + b);
-        }
-        else if(choice == 2){
-            int a = scn.nextInt();
-            int b = scn.nextInt();
-            System.out.println(a-b);
-        }
-        else if(choice == 3){
-            int a = scn.nextInt();
-            int b = scn.nextInt();
-            System.out.println(a*b);
-        }
-        else if(choice == 4){
-            int a = scn.nextInt();
-            int b = scn.nextInt();
-            System.out.println(a/b);
-        }
 
-        else{
-            System.out.println("Неверный выбор");
-        }
+
+
 
     }
 }
