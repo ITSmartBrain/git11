5b3da077274c2e77c1e149bad18d457eb6a4b25b
al.tukanov@gmail.com <al.tukanov@gmail.com>
2023-07-20 20:56:04 +0400

    добавлено меню

--- src/p1/Main.java
+++ src/p1/Main.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        System.out.println("""
+                1. Сложить
+                2. Вычесть
+                """);
         int a = scn.nextInt();
         int b = scn.nextInt();
         System.out.println(a+b);
