37b75a782fa69243b6a827b9f1611148787103e6
al.tukanov@gmail.com <al.tukanov@gmail.com>
2023-07-20 20:46:22 +0400

    scanner added

--- src/p1/Main.java
+++ src/p1/Main.java
@@ -1,9 +1,12 @@
 package p1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int a = 2;
-        int b = 3;
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
         System.out.println(a+b);
         if(a > b){
             System.out.println("test");
