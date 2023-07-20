aaa2393fc4d224366448aeb9bd6789fe3b548595
al.tukanov@gmail.com <al.tukanov@gmail.com>
2023-07-20 21:16:48 +0400

    menu added

--- src/p1/Main.java
+++ src/p1/Main.java
@@ -6,9 +6,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("""
-                
+                1. Сложить
+                2. Вычесть
                 """);
         int choice = scn.nextInt();
+        if(choice == 1){
+
+        }
+        else if(choice == 2){
+
+        }
+
 
 
 
